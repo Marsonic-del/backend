d1b6a8ae276e7b0e200a750bd75e746fb9774a63
Vladimir Marsov <marsov012@gmail.com>
2021-07-08 13:41:29 +0300

    Проектная работа 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,6 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new ForbiddenError('Необходима авторизация'));
-    // return res.status(403).send({ message: 'Необходима авторизация' });
   }
   // извлечём токен
   const token = JSON.parse(authorization.replace('Bearer ', ''));
@@ -16,11 +15,8 @@ module.exports = (req, res, next) => {
     // попытаемся верифицировать токен
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    throw new ForbiddenError('Необходима авторизация');
     // отправим ошибку, если не получилось
-    /* return res
-      .status(403)
-      .send({ message: 'Необходима авторизация' }); */
+    throw new ForbiddenError('Необходима авторизация');
   }
   req.user = payload; // записываем пейлоуд в объект запроса
 
