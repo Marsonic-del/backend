9afd456214c84eef77dc5634252c7aa7e9eefff8
Vladimir Marsov <marsov012@gmail.com>
2021-07-18 01:10:43 +0300

    cor

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -19,6 +19,7 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
   }
   next();
 };
